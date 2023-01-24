ab3db8f72beaf2d49a2331072e55395c88b276d6
lc6464 <64722907+lc6464@users.noreply.github.com>
2023-01-24 12:04:55 +0800

    优化正则表达式匹配逻辑

--- Processers/MainProcesser.cs
+++ Processers/MainProcesser.cs
@@ -11,25 +11,33 @@ public static class MainProcesser {
 	public static async Task ProcessReceivedMessageAsync(ReceivedMessage message) {
 		var echo = $"{DateTime.Now.Ticks}-{message.GroupId}-{message.UserId}-{message.MessageId}-{Random.Shared.NextString(16)}";
 		if (message.PostType == "message") {
-			if (message.MessageType == "group" && (message.RawMessage?.Contains("[CQ:redbag,title=") ?? false)) {
+			if (message.MessageType == "group" && message.RawMessage!.Contains("[CQ:redbag,title=")) {
 				await MessageTools.SendTextMessageAsync($"快到群 {message.GroupId} 中领取用户 {message.UserId} 发送的红包！", false, 1138_7791_74, echo).ConfigureAwait(false);
 			}
-			var aLiQueryRegexMatch = Regexes.ALiQueryRegex().Match(message.RawMessage!); // 获取阿梨相关查询功能的 Match
-			Match? biliUploaderQueryRegexMatch = null; // 获取B站UP主相关查询功能的 Match
-			if (!aLiQueryRegexMatch.Success) {
-				biliUploaderQueryRegexMatch = Regexes.BiliUploaderQueryRegex().Match(message.RawMessage!);
-			}
+
 			bool isGroupNotAnonymous = message.MessageType == "group" && message.SubType != "anonymous", // 群聊非匿名
 				isPrivateWithFriend = message.MessageType == "private" && message.SubType == "friend"; // 私聊好友
-			if ((isGroupNotAnonymous || isPrivateWithFriend) && aLiQueryRegexMatch.Success) {
+
+			var isLengthFrom8To80 = message.RawMessage!.Length is <= 80 and >= 8; // 判断字符数是否在8~80范围内
+
+			Match aLiQueryRegexMatch = Match.Empty, biliUploaderQueryRegexMatch = Match.Empty;
+			if ((isGroupNotAnonymous || isPrivateWithFriend) && isLengthFrom8To80) {
+				aLiQueryRegexMatch = Regexes.ALiQueryRegex().Match(message.RawMessage); // 获取阿梨相关查询功能的 Match
+				if (!aLiQueryRegexMatch.Success) {
+					biliUploaderQueryRegexMatch = Regexes.BiliUploaderQueryRegex().Match(message.RawMessage); // 获取B站UP主相关查询功能的 Match
+				}
+			}
+			
+			
+			if (aLiQueryRegexMatch.Success) {
 				// 如果是群消息且不是匿名消息，或者是私聊消息且是好友消息，且匹配到了阿梨相关查询功能的正则表达式
 				_logger.LogWithTime($"{message.GroupId} {message.UserId} 命中阿梨相关查询功能：{message.RawMessage}", LogLevel.Debug);
 				await ALiQueryProcesser.ProcessAsync(message, aLiQueryRegexMatch).ConfigureAwait(false);
-			} else if ((isGroupNotAnonymous || isPrivateWithFriend) && biliUploaderQueryRegexMatch is not null && biliUploaderQueryRegexMatch.Success) {
+			} else if (biliUploaderQueryRegexMatch.Success) {
 				// 如果是群消息且不是匿名消息，或者是私聊消息且是好友消息，且匹配到了B站UP主相关查询功能的正则表达式
 				_logger.LogWithTime($"{message.GroupId} {message.UserId} 命中B站UP主相关查询功能：{message.RawMessage}", LogLevel.Debug);
 				await BiliUploaderQueryProcesser.ProcessAsync(message, biliUploaderQueryRegexMatch).ConfigureAwait(false);
-			} else if (((isGroupNotAnonymous && message.GroupId == 5220_71644 && message.RawMessage!.Contains($"[CQ:at,qq={message.SelfId}]")) || (isPrivateWithFriend && message.UserId == 1138_7791_74)) && (message.RawMessage!.Contains("回调测试") || message.RawMessage!.Contains("撤回测试"))) {
+			} else if (((isGroupNotAnonymous && message.GroupId == 5220_71644 && message.RawMessage.Contains($"[CQ:at,qq={message.SelfId}]")) || (isPrivateWithFriend && message.UserId == 1138_7791_74)) && (message.RawMessage.Contains("回调测试") || message.RawMessage.Contains("撤回测试"))) {
 				_logger.LogWithTime($"{message.GroupId} {message.UserId} 命中回调或撤回测试：{message.RawMessage}", LogLevel.Debug);
 				var isGroup = message.GroupId is not null;
 				var messageText = $"[CQ:reply,id={message.MessageId}]{(isGroup ? $" [CQ:at,qq={message.UserId}]" : "")} 请等待约五秒（撤回测试可能会低于五秒）。";
@@ -61,7 +69,7 @@ public static class MainProcesser {
 						await MessageTools.SendTextMessageAsync(messageText, isGroup, isGroup ? message.GroupId : message.UserId, echo).ConfigureAwait(false);
 
 					}).ConfigureAwait(false);
-			} else if (isGroupNotAnonymous && message.RawMessage!.Contains($"[CQ:at,qq={message.SelfId}]")) {
+			} else if (isGroupNotAnonymous && message.RawMessage.Contains($"[CQ:at,qq={message.SelfId}]")) {
 				_logger.LogWithTime($"{message.GroupId} {message.UserId} 命中群聊 At：{message.RawMessage}", LogLevel.Debug);
 				SendMessage sendMessage = new() {
 					Action = "send_msg",
