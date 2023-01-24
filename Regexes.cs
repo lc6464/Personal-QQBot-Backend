17c6698deb4d46eff1a4715fb6e9c1fc4c225f58
lc6464 <64722907+lc6464@users.noreply.github.com>
2023-01-24 17:11:34 +0800

    修改阿梨正则表达式

--- Regexes.cs
+++ Regexes.cs
@@ -3,7 +3,7 @@
 namespace PersonalQQBotBackend.GeneratedRegexes;
 
 public static partial class Regexes {
-	[GeneratedRegex(@"查询阿梨(直播状态|最新稿件)", RegexOptions.Compiled)]
+	[GeneratedRegex(@"查询阿梨的?(直播状态|最新稿件)", RegexOptions.Compiled)]
 	public static partial Regex ALiQueryRegex();
 
 	[GeneratedRegex(@"查询(?:(?:B站)?UP主?|B站(?:用户|主播|Uploader))的?(?<type>直播状态|最新稿件)(?:：|:) *(?<uid>\d{1,18})(?:$|\D)", RegexOptions.Compiled | RegexOptions.IgnoreCase)]
